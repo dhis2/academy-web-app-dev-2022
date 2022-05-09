9383f3e2131a0f4e9d37aa4932449081d27d1c94
Birk Johansson <birk@dhis2.org>
2022-05-09 13:21:05 +0200

    fix: update 05 and 06 exercises

--- workshop/06-generic-dhis2-apps/datastore-solution/d2.config.js
+++ workshop/06-generic-dhis2-apps/datastore-solution/d2.config.js
@@ -1,9 +1,8 @@
 const config = {
     type: 'app',
-    dataStoreNamespace: 'my-custom-app-namespace-1234',
 
     entryPoints: {
-        app: './src/App',
+        app: './src/App.js',
     },
 }
 
--- workshop/06-generic-dhis2-apps/datastore/d2.config.js
+++ workshop/06-generic-dhis2-apps/datastore/d2.config.js
@@ -2,8 +2,9 @@ const config = {
     type: 'app',
     dataStoreNamespace: 'my-custom-app-namespace-1234',
 
+
     entryPoints: {
-        app: './src/App',
+        app: './src/App.js',
     },
 }
 
