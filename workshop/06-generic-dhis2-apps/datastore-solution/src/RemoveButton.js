8c0631583521656b12a150a3d7687e3219c82b83
Austin McGee <947888+amcgee@users.noreply.github.com>
2022-05-12 09:07:12 +0200

    feat: update deps and add PWA support to exercise

--- workshop/06-generic-dhis2-apps/datastore-solution/src/RemoveButton.js
+++ workshop/06-generic-dhis2-apps/datastore-solution/src/RemoveButton.js
@@ -1,14 +1,18 @@
-import { useSavedObject } from '@dhis2/app-service-datastore'
-import { Button } from '@dhis2/ui'
-import { removeButton } from './App.module.css'
+import { useOnlineStatus } from "@dhis2/app-runtime";
+import { useSavedObject } from "@dhis2/app-service-datastore";
+import { Button } from "@dhis2/ui";
+import { removeButton } from "./App.module.css";
 
 const RemoveButton = ({ id }) => {
-    const [, { remove }] = useSavedObject(id)
-    return <span className={removeButton}>
-        <Button dense destructive onClick={() => remove(id)}>
-            -
-        </Button>
+  const [, { remove }] = useSavedObject(id);
+  const { offline } = useOnlineStatus();
+  return (
+    <span className={removeButton}>
+      <Button disabled={offline} dense destructive onClick={() => remove(id)}>
+        -
+      </Button>
     </span>
-}
+  );
+};
 
-export default RemoveButton
+export default RemoveButton;
